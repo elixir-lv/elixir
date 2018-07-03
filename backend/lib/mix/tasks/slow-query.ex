f09fabd42042e841f95e0be666ee922b69f38622
Janis Rullis <janis.rullis.it@gmail.com>
2018-07-03 19:17:45 +0300

    Backend: Add mix slow_query to test DB timeouts

---
+++ backend/lib/mix/tasks/slow-query.ex
@@ -0,0 +1,18 @@
+defmodule Mix.Tasks.SlowQuery do
+  import Mix.Ecto
+
+  @shortdoc "Simulate a slow query to test DB timeouts."
+
+  @moduledoc """
+  * mix slow_query
+  """
+  use Mix.Task
+
+  def run(args) do
+    ensure_started(ElixirBackend.Repo, [])
+    query = "SELECT sleep(60)"
+    IO.inspect  Ecto.Adapters.SQL.query!(ElixirBackend.Repo, query, [])
+
+    # [error] Mariaex.Protocol (#PID<0.216.0>) disconnected: ** (DBConnection.ConnectionError) client #PID<0.74.0> timed out because it checked out the connection for longer than 30000ms
+  end
+end
