fa68899a6722b86e8d4d3d69fa5d47c1b9824640
Janis Rullis <janis.rullis.it@gmail.com>
2018-07-10 20:26:42 +0300

    Docker-compose: Add PhpMyAdmin at 172.60.1.14

--- backend/lib/elixir_backend_web/router.ex
+++ backend/lib/elixir_backend_web/router.ex
@@ -7,5 +7,8 @@ defmodule ElixirBackendWeb.Router do
 
   scope "/api", ElixirBackendWeb do
     pipe_through :api
+
+    resources "/users", UserController, except: [:new, :edit]
+    resources "/posts", PostController, except: [:new, :edit]
   end
 end
