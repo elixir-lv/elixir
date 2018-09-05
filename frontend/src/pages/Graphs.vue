17158d924a1f5b09ac971acf38acf84c95ead978
Janis Rullis <janis.rullis.it@gmail.com>
2018-09-05 21:12:14 +0300

    vue: Add /graphs

---
+++ frontend/src/pages/Graphs.vue
@@ -0,0 +1,10 @@
+<template>
+	<default-layout>
+		<graphs></graphs>
+	</default-layout>
+</template>
+<script>
+	import defaultLayout from '../layouts/Default.vue';
+	import graphs from '../components/graphs.vue';
+	export default {components: {defaultLayout, graphs}}
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,6 +7,7 @@ const router = new Router({mode: 'history', base: '/', routes: [
 		{path: '/posts/:id', name: 'PostPage', component: () => import(/* webpackChunkName: "PostPage" */ '../pages/Post.vue')},
 		{path: '/map', name: 'MapPage', component: () => import(/* webpackChunkName: "MapPage" */ '../pages/Map.vue')},
 		{path: '/contacts', name: 'ContactsPage', component: () => import(/* webpackChunkName: "ContactsPage" */ '../pages/Contacts.vue')},
+		{path: '/graphs', name: 'GraphsPage', component: () => import(/* webpackChunkName: "GraphsPage" */ '../pages/Graphs.vue')},
 	]
 });
 export default router;
