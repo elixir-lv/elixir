36adb9619c4b92aa5f2fa6b809b39c5c80536ff5
Janis Rullis <janis.rullis.it@gmail.com>
2018-09-07 15:23:48 +0300

    Update app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
-import VueResource from "vue-resource";
-Vue.use(VueResource);
 import router from "./router";
 import App from './App.vue';
-new Vue({http: {root: 'http://api.elixir.local'}, router, render: h => h(App)}).$mount("#app");
+import VueResource from "vue-resource";
+Vue.use(VueResource);
+
+Vue.options.http.root = 'http://api.elixir.local';
+new Vue(router, render: h => h(App)}).$mount("#app");
