51005907224743cfd74bd7dbaf21af7b10f26bcc
Janis Rullis <janis.rullis.ite@gmail.com>
2018-12-21 21:43:04 +0200

    backend/incremental-slug::getSlug(string): Add iex examples and a type-spec

--- backend/lib/backend/incremental-slug.ex
+++ backend/lib/backend/incremental-slug.ex
@@ -2,6 +2,8 @@ defmodule Backend.IncrementalSlug do
 
   @moduledoc """
   Provide unique slugs by appending an increment if the slugs has already been used.
+
+  Uses [github.com/h4cc/slugger](https://github.com/h4cc/slugger)
   """
 
   import Ecto.Query, warn: false
@@ -17,16 +19,22 @@ defmodule Backend.IncrementalSlug do
   def put(changeset, module, fromField, toField) when is_nil(changeset) or is_nil(module), do: changeset
   def put(changeset, module, fromField, toField), do: getSlugFromField(changeset, module, fromField, toField) |> putSlug(changeset, toField)
 
-  # Get a unique slug by convertig the passed value (fromField).
+  @doc """
+  Get a unique slug by convertig the passed value (fromField).
+  """
   def getSlugFromField(changeset, module, fromField \\ @incremental_slug.from_field, toField \\ @incremental_slug.to_field)
   def getSlugFromField(changeset, module, fromField, toField), do: get_change(changeset, fromField) |> getUniq(get_change(changeset, :id), module, toField)
 
-  # Get a unique slug.
+  @doc """
+  Get a unique slug.
+  """
   def getUniq(string, id, module, toField \\ @incremental_slug.to_field)
   def getUniq(string, id, module, toField) when is_nil(string) or id == 0 or is_nil(module), do: nil
   def getUniq(string, id, module, toField), do: string |> getSlug |> makeSlugUnique(id, module, toField)
 
-  # Make sure that the passed slug will be unique.
+  @doc """
+  Make sure that the passed slug will be unique.
+  """
   def makeSlugUnique(slug, id, module, toField \\ @incremental_slug.to_field)
   def makeSlugUnique(slug, id, module, toField), do: isTaken(slug, id, module, toField) |> makeSlugUnique(slug, id, module, toField)
   def makeSlugUnique(taken, slug, id, module, toField) when taken === true, do: getIncrement(slug, id, module, toField) |> concat(slug)
@@ -34,30 +42,55 @@ defmodule Backend.IncrementalSlug do
 
   def concat(increment, string), do: "#{string}-#{increment}"
 
-  # Get a slug that is genererated from the passed string.
+  @doc """
+  Get a slug that is genererated from the passed string.
+
+  Trim and then pass it to [`Slugger.slugify()`](https://github.com/h4cc/slugger)
+
+  ## Examples
+
+      iex> Backend.IncrementalSlug.getSlug("Some title")
+      "Some-title"
+
+      iex> Backend.IncrementalSlug.getSlug(" z e ā Č Ф А - Б В Г	Д š \ / * ^ % ! + ) |")
+      "z-e-a-C-F-A-B-V-GD-s-or"
+  """
+  @spec getSlug(string :: nil) :: nil
   def getSlug(string) when is_nil(string), do: nil
+
+   @spec getSlug(string :: String.t()) :: String.t()
   def getSlug(string), do: string |> String.trim() |> Slugger.slugify()
 
-  # Check if another item has taken this slug.
+  @doc """
+  Check if another item has taken this slug.
+  """
   def isTaken(string, id, module, toField \\ @incremental_slug.to_field)
   def isTaken(string, id, module, toField) when is_nil(string) or id == 0 or is_nil(module), do: nil
   def isTaken(string, id, module, toField), do: getCount(string, id, module, toField) > 0
 
-  # Get the count of how many items has used this slug (with or without an increment).
+  @doc """
+  Get the count of how many items has used this slug (with or without an increment).
+  """
   def getCount(string, id, module, toField \\ @incremental_slug.to_field)
   def getCount(string, id, module, toField), do: module |> select(count("*")) |> limit(1) |> where([a], field(a, ^toField) == ^string) |> exlcudeSelf(id) |> Repo.one
 
-  # Do not include the current item when looking for item's that has used this slug.
+  @doc """
+  Do not include the current item when looking for item's that has used this slug.
+  """
   def exlcudeSelf(query, id) when is_nil(id), do: query
   def exlcudeSelf(query, id), do: query |> where([a], a.id != ^id)
 
-  # Get the increment to add to the slug so it would be unique.
+  @doc """
+  Get the increment to add to the slug so it would be unique.
+  """
   def getIncrement(string, id, module, toField \\ @incremental_slug.to_field)
   def getIncrement(string, id, module, toField), do: getLastIncrement(string, id, module, toField) |> getIncrement
   def getIncrement(lastIncrement), do: lastIncrement + 1
 
-  # Get the increment from the last item with this slug. Like post-1 increment is 1.
-  # If no item can be found then 0 will be returned.
+  @doc """
+  Get the increment from the last item with this slug. Like post-1 increment is 1.
+  If no item can be found then 0 will be returned.
+  """
   def getLastIncrement(string, id, module, toField \\ @incremental_slug.to_field)
   def getLastIncrement(string, id, module, toField), do: find(string, id, module, toField) |> getLastIncrement
   def getLastIncrement(string) when is_nil(string), do: 0
@@ -70,8 +103,10 @@ defmodule Backend.IncrementalSlug do
   def selectField(module, toField \\ @incremental_slug.to_field)
   def selectField(module, toField), do: module |> select([a], field(a, ^toField))
 
-  # Search for this slug that ends with '-' and exactly 1 character.
-  # See https://dev.mysql.com/doc/refman/8.0/en/pattern-matching.html
+  @doc """
+  Search for this slug that ends with '-' and exactly 1 character.
+  See https://dev.mysql.com/doc/refman/8.0/en/pattern-matching.html
+  """
   def whereFieldWithIncrement(query, string, toField \\ @incremental_slug.to_field)
   def whereFieldWithIncrement(query, string, toField), do: query |> where([a], like(field(a, ^toField), ^"#{string}-_"))
 
